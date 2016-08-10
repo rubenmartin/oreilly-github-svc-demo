642b1a6b650fc16e100fc0ea4a2c6c2c117658a8
rubenmartin <rmartinramos@soldevelo.com>
2016-08-10 22:23:12 +0200

    Adding new changes for Regex

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
-module.exports = function hackerCase(text) {
+module.exports = function hackerCase(text, replaceMap) {
   var upperCase = false;
 
-  return text.replace(/\w/g, function(char) {
-    return char[(upperCase = !upperCase) ? 'toLocaleUpperCase' : 'toLocaleLowerCase']();
+  var result = text.replace(/\w/g, function(char){
+  	 return char[(uppercase = !uppercase) ? 'toLocaleUpperCase': 'toLocaleLowerCase']();
   });
+
+  if(replaceMap){
+  	 for(var src in replaceMap){
+  	 	result = result.replace(new RegExp(src, 'gi'), String(replaceMap[src]));
+  	 }
+  }
+
+  return result;
 };
