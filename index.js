f912c3fcc41c2684e9aee33f5c209d2722ed5889
rubenmartin <rmartinramos@soldevelo.com>
2016-08-10 22:57:23 +0200

    Better replaceMap

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 module.exports = function hackerCase(text, replaceMap) {
   var upperCase = false;
 
-  var result = text.replace(/\w/g, function(char){
+  var result = text.replace(/\w/g, function(char) {
   	 return char[(uppercase = !uppercase) ? 'toLocaleUpperCase': 'toLocaleLowerCase']();
   });
 
-  if(replaceMap){
-  	 for(var src in replaceMap){
+  if('object' === typeof replaceMap) {
+  	 for(var src in replaceMap) {
   	 	result = result.replace(new RegExp(src, 'gi'), String(replaceMap[src]));
   	 }
   }
