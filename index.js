cf56a2320265b9b1fed447f3f4d44493872c1599
rubenmartin <rmartinramos@soldevelo.com>
2016-08-10 21:46:08 +0200

    Simplify regex change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function hackerCase(text) {
   var upperCase = false;
 
-  return text.replace(/\w/g, function(char) {
+  return text.replace(/./g, function(char) {
     return char[(upperCase = !upperCase) ? 'toLocaleUpperCase' : 'toLocaleLowerCase']();
   });
 };
